a2b64dfa498494961f5d32b8e8a08bc33cf68f96
chamodya <chamodyarajapaksha1@gmail.com>
2025-04-08 09:54:24 +0530

    update header with material ui

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,29 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @Component({
   selector: 'app-header',
   imports: [
-    CommonModule,
-    FormsModule,
-    MatSlideToggleModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatDividerModule,
-    MatCardModule
+    MatToolbarModule, MatButtonModule, MatIconModule
   ],
-
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
