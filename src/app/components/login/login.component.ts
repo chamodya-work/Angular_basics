213cf82422e0789009d746009b8788fd5d722883
chamodya <chamodyarajapaksha1@gmail.com>
2025-04-20 00:28:30 +0530

    updated with the login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +7,14 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+
+  }
+  login(loginForm: NgForm) {
+    console.log("login succesfuly");
+    console.log(loginForm.value);
+  }
 
 }
