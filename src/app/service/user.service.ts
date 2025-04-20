abc9005a1c31a557f32cca7bf4763b875cb316d4
chamodya <chamodyarajapaksha1@gmail.com>
2025-04-20 19:05:08 +0530

    updated login ui

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,9 +6,15 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
   BASE_URL = "http://localhost:8083";
+
+  requestHeader = new HttpHeaders(
+    {
+      "No-Auth": "True"
+    }
+  );
   constructor(private httpClient: HttpClient) { }
 
   public login(loginData: any) {
-    return this.httpClient.post(this.BASE_URL + "/authenticate", loginData)
+    return this.httpClient.post(this.BASE_URL + "/authenticate", loginData, { headers: this.requestHeader });
   }
 }
