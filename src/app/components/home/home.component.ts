ea5d47a44b80084df4d76e25b94cb9ad0b600cab
chamodya <chamodyarajapaksha1@gmail.com>
2025-04-22 23:57:42 +0530

    updated getstart routes to login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuService } from '../../service/menu.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
